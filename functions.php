9aca5a9af8e09554e323dfb927357ac95ca40ed3
Yulia V <yuliavoevodskaya@Yulias-MacBook-Pro.local>
2016-03-22 01:09:38 +0000

    shorthand works, except image scaling
    shorthand works, except image scaling

--- functions.php
+++ functions.php
@@ -11,8 +11,7 @@ function theme_enqueue_styles() {
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 
-add_shortcode('wp_caption', 'MY_VERY_OWN_img_caption_shortcode');
-add_shortcode('caption', 'MY_VERY_OWN_img_caption_shortcode');
+add_shortcode('yu_caption', 'MY_VERY_OWN_img_caption_shortcode');
 
 /**
  * The Caption shortcode.
@@ -21,7 +20,7 @@ add_shortcode('caption', 'MY_VERY_OWN_img_caption_shortcode');
  * filter is 'img_caption_shortcode' and passes an empty string, the attr
  * parameter and the content parameter values.
  *
- * The supported attributes for the shortcode are 'id', 'align', 'width', and
+ * The supported attributes for the shortcode are 'height', 'width', and
  * 'caption'.
  *
  * @since 2.6.0
@@ -33,12 +32,17 @@ add_shortcode('caption', 'MY_VERY_OWN_img_caption_shortcode');
 function MY_VERY_OWN_img_caption_shortcode($attr, $content = null) {
 
 	extract(shortcode_atts(array(
+		'height' => '',
 		'width'	=> '',
 		'caption' => ''
 	), $attr));
 
-	return '<table width="' . (10 + (int) $width) . '"> <tr> <td> '
-	. do_shortcode( $content ) . '</td></tr><tr> <td> <p class="wp-caption-text">' . $caption . '</p></td></tr></table>';
+	if ( $height ) $height = 'height="' . (int) $height . '" ';
+
+	return '<table width="' . (10 + (int) $width) . '"> <tr> <td width="' . (10 + (int) $width) . '"> <a href="'
+	. $content . '"><img src="' . $content . '" alt=" ' . str_replace(array('<br />','<br/>','<br>'), '', $caption) . ' " width="' . (int) $width . '" ' . $height 
+	. ' /></a> </td></tr><tr> <td width="' . (10 + (int) $width)
+	. '"> <p class="wp-caption-text">' . $caption . '</p></td></tr></table>';
 }
 
 ?>
