4b7f31ff20455e3a3db304ebd3c3eaabacf1aecc
Yulia V <yuliavoevodskaya@Yulias-MacBook-Pro.local>
2016-03-22 00:06:04 +0000

    adding caption shorthand
    adding caption shorthand

--- functions.php
+++ functions.php
@@ -1,23 +1,44 @@
 <?php
 function theme_enqueue_styles() {
 
-        $parent_style = 'parent-style';
+    $parent_style = 'parent-style';
 
-            wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-            wp_enqueue_style( 'child-style',
-                        get_stylesheet_directory_uri() . '/style.css',
-                                array( $parent_style )
-                                    );
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style )
+    );
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 
+add_shortcode('wp_caption', 'MY_VERY_OWN_img_caption_shortcode');
+add_shortcode('caption', 'MY_VERY_OWN_img_caption_shortcode');
 
+/**
+ * The Caption shortcode.
+ *
+ * Allows a plugin to replace the content that would otherwise be returned. The
+ * filter is 'img_caption_shortcode' and passes an empty string, the attr
+ * parameter and the content parameter values.
+ *
+ * The supported attributes for the shortcode are 'id', 'align', 'width', and
+ * 'caption'.
+ *
+ * @since 2.6.0
+ *
+ * @param array $attr Attributes attributed to the shortcode.
+ * @param string $content Optional. Shortcode content.
+ * @return string
+ */
+function MY_VERY_OWN_img_caption_shortcode($attr, $content = null) {
 
-add_filter('woocommerce_currency_symbol','aed_change_symbol',1,2);
-function aed_change_symbol($currency_symbol, $currency) {
-        if($currency == 'AED')
-                    $currency_symbol = 'AED';
-            return $currency_symbol;
+	extract(shortcode_atts(array(
+		'width'	=> '',
+		'caption' => ''
+	), $attr));
+
+	return '<table width="' . (10 + (int) $width) . '"> <tr> <td> '
+	. do_shortcode( $content ) . '</td></tr><tr> <td> <p class="wp-caption-text">' . $caption . '</p></td></tr></table>';
 }
 
 ?>
