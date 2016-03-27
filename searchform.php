b6df3bce2163d846b8860793f41073eab1aad834
Yulia V <yuliavoevodskaya@Yulias-MacBook-Pro.local>
2016-03-27 20:16:26 +0100

    header works
    header works

--- searchform.php
+++ searchform.php
@@ -1,8 +1,8 @@
-<h3 class="widget-title">Search This Website</h3>
 <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>" >	
+	<h3 class="widget-title" style="margin-bottom: 20px">Search This Website</h3>
 	<p id="search-p">
 	<label id="s-label" for="s" style="clip: rect(1px 1px 1px 1px); position: absolute; height: 1px; width: 1px; overflow: hidden;"> Keyword(s) </label>
 		<input type="text" value="" placeholder="Keyword(s)" name="s" id="s" />
 	</p>
-	<input type="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'pinboard' ); ?>" />
+	<input type="submit" id="searchsubmit" required="required" class="required" value="<?php esc_attr_e( 'Search', 'pinboard' ); ?>" />
 </form>
