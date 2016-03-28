8f19b0e6051bd459520a552960abfe4f0732b345
Yulia V <yuliavoevodskaya@Yulias-MacBook-Pro.local>
2016-03-28 09:32:38 +0100

    header tidy-up
    header tidy-up

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
 <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>" >	
-	<h3 class="widget-title" style="margin-bottom: 20px">Search This Website</h3>
+	<h3 class="widget-title">Search This Website</h3>
 	<p id="search-p">
 	<label id="s-label" for="s" style="clip: rect(1px 1px 1px 1px); position: absolute; height: 1px; width: 1px; overflow: hidden;"> Keyword(s) </label>
 		<input type="text" value="" required="required" class="required" placeholder="Keyword(s)" name="s" id="s" />
